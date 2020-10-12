7975c75b9966c0a5b7e7a40e059cfe37af3088fc
EkaterinaChernyugova <72301337+EkaterinaChernyugova@users.noreply.github.com>
2020-10-12 21:43:26 +0700

    Add files via upload

--- Goods.cpp
+++ Goods.cpp
@@ -158,7 +158,6 @@ char* Goods::ToStringNumber()
 char* Goods::ToString() 
 {
 	int  k, l, i;
-
 	char* strPrice;
 	char* strNumber;
 	strPrice = ToStringPrice();
@@ -168,70 +167,24 @@ char* Goods::ToString()
 	l = l + k;
 	k = strlen(strNumber);
 	l = l + k;
-	char* STR = (char*)malloc(sizeof(int) * l);
-
-	STR[0] = 'N';
-	STR[1] = 'a';
-	STR[2] = 'm';
-	STR[3] = 'e';
-	STR[4] = ':';
-	STR[5] = ' ';
-	k = strlen(name);
-	int z = 6;
-	for (i = 0; i < k ; i++)
+	char STR[100];
+	strcpy_s(STR, "Name:");
+	strcat_s(STR, name);
+	strcat_s(STR, " Date: ");
+	strcat_s(STR, date);
+	strcat_s(STR, " Price ");
+	strcat_s(STR, strPrice);
+	strcat_s(STR, " Number ");
+	strcat_s(STR, strNumber);
+	l = strlen(STR);
+	char* ST = (char*)malloc(sizeof(int) * l);
+	for (i = 0; i < l; i++)
 	{
-		STR[z + i] = name[i];
+		ST[i] = STR[i];
 	}
-	z = z + k;
-
-	STR[z + 0] = ' ';
-	STR[z + 1] = 'D';
-	STR[z + 2] = 'a';
-	STR[z + 3] = 't';
-	STR[z + 4] = 'e';
-	STR[z + 5] = ':';
-	STR[z + 6] = ' ';
-	z = z + 7;
-	k = strlen(date);
-	for (i = 0; i < k; i++)
-	{
-		STR[z + i] = date[i];
-	}
-	z = z + k;
-
-	STR[z + 0] = ' ';
-	STR[z + 1] = 'P';
-	STR[z + 2] = 'r';
-	STR[z + 3] = 'i';
-	STR[z + 4] = 'c';
-	STR[z + 5] = 'e';
-	STR[z + 6] = ':';
-	STR[z + 7] = ' ';
-	z = z + 8;
-	k = strlen(strPrice);
-	for (i = 0; i < k; i++)
-	{
-		STR[z + i] = strPrice[i];
-	}
-	z = z + k;
-
-	STR[z + 0] = ' ';
-	STR[z + 1] = 'N';
-	STR[z + 2] = 'u';
-	STR[z + 3] = 'm';
-	STR[z + 4] = 'b';
-	STR[z + 5] = 'e';
-	STR[z + 6] = 'r';
-	STR[z + 7] = ':';
-	STR[z + 8] = ' ';
-	z = z + 9;
-	k = strlen(strNumber);
-	for (i = 0; i < k; i++)
-	{
-		STR[z + i] = strNumber[i];
-	}
-	z = z + k;
-	STR[z] = '\0';
-	return STR;
+	ST[l - 1] = '\0';
+	return ST;
 }
+
+
 
